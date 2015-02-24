fc81573c62bc46aec3182da6dc162dab655dfb21
Saurav Kumar <2020saurav@gmail.com>
2015-02-24 23:39:47 +0530

    Added converter to convert parsedump into Parse Tree: token value not done

--- src/converter.py
+++ src/converter.py
@@ -1,45 +1,42 @@
-data='''
-Stmt([Function(None, 'factorial', ('n',), (), 0, None, Stmt([If([(Compare(Name('n'), [('<', Const(2))]), Stmt([Return(Const(1))]))], None), Return(Mul((Name('n'), CallFunc(Name('factorial'), [Sub((Name('n'), Const(1)))], None, None))))])), Print(CallFunc(Name('factorial'), [Const(5)], None, None), None)])'''
-output="""
-Stmt(
-		[
-			Function
-				(
-					None, 'factorial', ('n',), (), 0, None, Stmt(
-																	[
-																		If(
-																			[
-																				(Compare
-																					(Name('n'), [('<', Const(2))]
-																					), 
-																					Stmt([Return(Const(1))])
-																				)
-																			],
-																			None
-																		  ), 
-																		Return(
-																				Mul(
-																					(
-																						Name('n'), 
-																						CallFunc
-																						(
-																							Name('factorial'), 
-																							[Sub((Name('n'), Const(1)))], 
-																							None, None
-																						)
-																					)
-																				   )
-																			  )
-																	]
-															 	)
-				), 
-			
-			Print(
-					CallFunc(
-								Name('factorial'), [Const(5)], None, None
-							), 
-					None
-				)
-		]
-	)
-"""
+def isAction(x):
+	first = x[:x.find(":")]
+	if first=="Action ":
+		return True
+def isReduce(x):
+	third = x[x.find(":")+2:x.find(":")+3]
+	if third == "R":
+		return True
+	else:
+		return False
+def getRule(x):
+	rule = x[x.find("[")+1:x.find("]")]
+	return rule
+def left(x):
+	return x[:x.find(" ")]
+def right(x):
+	children = x[x.find("->")+3:]
+	children = children.split()
+	return children
+lines = open('dump').readlines()
+lines = [line for line in lines if isAction(line)]
+lines = reversed(lines)
+stack = [] # actually a list
+nodeId = 0
+
+stack.append(("program",0,None))
+print "digraph G \n{\n"
+print "\tnode0 [label=\"program\"];"
+for line in lines:
+	if isReduce(line):
+		item = stack.pop()
+		rule = getRule(line)
+		children = right(rule)
+		for child in children:
+			nodeId+=1
+			stack.append((child,nodeId,item[2]))
+			print "\tnode"+str(nodeId)+" [label= \""+child+"\"];"
+			print "\tnode"+str(item[1])+" -> node"+str(nodeId)+";"
+	else:
+		stack.pop()
+		pass
+print "}"
--- test/test1.py
+++ test/test1.py
@@ -1,11 +1,2 @@
-import sys
-def factorial (num):
-	if(num<20):
-		return 21
-	else:
-		return num*factorial(num-1)
-
 for index in range(10,25):
 	print factorial(index)
-
-print "Hello"
