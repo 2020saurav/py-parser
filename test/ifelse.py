c0bcd86471f39d4394d8b210ff1e80a48160d254
Abhilash Kumar <abhi.iitk16@gmail.com>
2015-03-07 19:28:26 +0530

    testes basic grammar , found few bugs

---
+++ test/ifelse.py
@@ -0,0 +1,7 @@
+a , b = 0.0 , 0
+if a > 2 : 
+	pass
+elif a == 5 :
+	pass
+else :
+	pass
---
+++ test/while.py
@@ -0,0 +1,4 @@
+a = 1+2^7*5-11|24
+#while a > 0 && a == 5: this is not giving any output , double conditioning
+while a > 0:
+	break
