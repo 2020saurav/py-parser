2961812d14a28cfff83c1b30d69eceff99d88ab1
Saurav Kumar <2020saurav@gmail.com>
2015-03-11 14:50:29 +0530

    Dont remember changes

--- test/test1.py
+++ test/test1.py
@@ -1,1 +1,18 @@
-a= ["a","b"]
+def foo(a):
+	print a
+	print a+1
+
+def main():
+	a = 42
+	arr = [ 43, 34,
+		85,
+				89,
+	77
+	]
+	foo(a)
+	b = "my name is\np ksaurav"
+	print b
+	#comment to be ignored by the lexer
+
+if __name__=='__main__':
+	main()
